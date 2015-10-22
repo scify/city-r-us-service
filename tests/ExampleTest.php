ad89e57c36ad2f8d9ed3e04b72e6ba7f4041bb0a
xristv <xrist.ven@gmail.com>
2015-10-22 13:31:53 +0300

    Added README.md

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,11 +7,30 @@ class ExampleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testBasicExample()
-	{
-		$response = $this->call('GET', '/');
+//	public function testBasicExample()
+//	{
+//		$response = $this->call('GET', '/');
+//
+//		$this->assertEquals(200, $response->getStatusCode());
+//	}
+    public function dummyTest_VariableHasAlwaysValueOfOne()
+    {
+        $variable = 1;
 
-		$this->assertEquals(200, $response->getStatusCode());
-	}
+        //$this->assertEquals(1,$variable );
+
+        $this->assertTrue(1== $variable);
+
+    }
+
+    //proposed test methods
+
+    //testLogin
+
+    //test_Missions_Retrieve
+
+    //test_Missions_Create
+
+    //test_UploadRoute  
 
 }
