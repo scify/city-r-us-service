1be7fea5338cedab258e2340d019ed9a4371e8c4
Nick Zorbas <nickzorb@gmail.com>
2016-03-04 09:47:57 +0200

    Added link to site

--- resources/views/template/email.blade.php
+++ resources/views/template/email.blade.php
@@ -3,10 +3,10 @@
         <title>City R Us - @yield('title')</title>
     </head>
     <body style='width: 100%; height: 100%; background: white; text-align: center; padding: 100px 0;'>
-        <div style='width: 40%; min-width: 400px; height: auto; padding: 20px; text-align: left; margin: auto;'>
-            <img src="../../../public/img/logo.png" alt="City R Us" style='width: 25%; height: auto; display: inline-block'/>
-            <p style='font-size: 1.5em; color: rgba(54,106,169,0.9)'>City R Us</p>
-        </div>
+        <a href="{{URL::to('/')}}" style='display: block; width: 40%; min-width: 400px; height: auto; padding: 20px; text-align: left; margin: auto;'>
+            <img src="{{asset('img/logo.png')}}" alt='City R Us' style='width: 25%; height: auto; display: inline-block;'>
+            <p style='font-size: 1.5em; color: rgba(54,106,169,0.9); display: inline-block;'>City R Us</p>
+        </a>
         <div style='width: 40%; min-width: 400px; height: auto; padding: 20px; background: rgba(54,106,169,0.9); text-align: left; margin: auto; color: white; font-size: 1.25em;'>
             @yield('content')
         </div>
