c046d09fb0fc702f13bb85c56decafbc3a7f4908
Nick Zorbas <nickzorb@gmail.com>
2016-03-04 09:40:27 +0200

    Making mail template prettier

--- resources/views/template/email.blade.php
+++ resources/views/template/email.blade.php
@@ -2,8 +2,12 @@
     <head>
         <title>City R Us - @yield('title')</title>
     </head>
-    <body style='width: 100%; height: 100%; background: white; text-align: center;'>
-        <div style='width: 400px; height: auto; padding: 20px; margin-top: 50px; background: lightblue; text-align: center;'>
+    <body style='width: 100%; height: 100%; background: white; text-align: center; padding: 100px 0;'>
+        <div style='width: 40%; min-width: 400px; height: auto; padding: 20px; text-align: left; margin: auto;'>
+            <img src="../../../public/img/logo.png" alt="City R Us" style='width: 25%; height: auto; display: inline-block'/>
+            <p style='font-size: 1.5em; color: rgba(54,106,169,0.9)'>City R Us</p>
+        </div>
+        <div style='width: 40%; min-width: 400px; height: auto; padding: 20px; background: rgba(54,106,169,0.9); text-align: left; margin: auto; color: white; font-size: 1.25em;'>
             @yield('content')
         </div>
     </body>
